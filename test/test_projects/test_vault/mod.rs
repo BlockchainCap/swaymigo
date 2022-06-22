602569157703b2f02969e4899c005419e6e7e3c1
Ryan Sproule <rsproule23@gmail.com>
2022-06-22 08:54:11 -0700

    update test boilerplate

--- test/test_projects/test_vault/mod.rs
+++ test/test_projects/test_vault/mod.rs
@@ -3,33 +3,33 @@ use fuels_abigen_macro::abigen;
 
 // Load abi from json
 abigen!(
-    VoteToken,
-    "test_projects/test_vote_token/out/debug/test_vote_token-abi.json"
+    VaultTest,
+    "test_projects/test_vault/out/debug/test_vault-abi.json"
 );
 
-async fn get_contract_instance() -> (VoteToken, ContractId) {
+async fn get_contract_instance() -> (VaultTest, ContractId) {
     // Launch a local network and deploy the contract
     let wallet = launch_provider_and_get_single_wallet().await;
 
     let id = Contract::deploy(
-        "test_projects/test_vote_token/out/debug/test_vote_token.bin",
+        "test_projects/test_vault/out/debug/test_vault.bin",
         &wallet,
         TxParameters::default(),
     )
     .await
     .unwrap();
 
-    let instance = VoteToken::new(id.to_string(), wallet);
+    let instance = VaultTest::new(id.to_string(), wallet);
 
     (instance, id)
 }
 
 #[tokio::test]
-async fn test_snapshots_full() {
+async fn test_deposit_to_vault() {
     let (_instance, _id) = get_contract_instance().await;
 }
 
 #[tokio::test]
-async fn should_fail_block_not_mined() {
+async fn test_withdraw_from_vault() {
     let (_instance, _id) = get_contract_instance().await;
 }
